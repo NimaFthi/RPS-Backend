97dd717536291446326a8664475ea93a28df115d
Nima <Nima.fthi78@gmail.com>
2025-11-15 11:20:09 +0330

    [database] match data migration

---
+++ RPS-Backend/Migrations/20251115070619_Matchdata.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace RPS_Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class Matchdata : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MatchData",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    MatchId = table.Column<int>(type: "integer", nullable: false),
+                    MatchName = table.Column<string>(type: "text", nullable: false),
+                    Entry = table.Column<int>(type: "integer", nullable: false),
+                    Prize = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MatchData", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MatchData");
+        }
+    }
+}
